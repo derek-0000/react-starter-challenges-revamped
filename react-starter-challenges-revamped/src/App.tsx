b61c027c0e6888a24a0a9e579595951e0561f0e1
Vanessa <karenvanesssa.gil@gmail.com>
2024-11-14 12:17:22 -0600

    add Layout present on all pages

--- react-starter-challenges-revamped/src/App.tsx
+++ react-starter-challenges-revamped/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Tic_tac_toe from "./pages/Tic_tac_toe";
+import { Tic_tac_toe } from "./pages/Tic_tac_toe";
 import Video from "./pages/Video";
 import Github_clone from "./pages/Github_clone";
 import Default from "./pages/Default";
@@ -9,9 +9,8 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Layout />
       <Routes>
-        <Route path="/" element={<Layout />} />
         <Route path="/tic_tac_toe" element={<Tic_tac_toe />} />
         <Route path="/video" element={<Video />} />
         <Route path="/github_clone" element={<Github_clone />} />
